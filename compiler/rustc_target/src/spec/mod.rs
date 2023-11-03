3a7f8528975d486df0b16eb497a4fa343a7bc4e9
Andelf <andelf@gmail.com>
2023-11-03 21:18:01 +0800

    Add rv32ec, use cunstom LLVM

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -1636,6 +1636,7 @@ supported_targets! {
 
     ("x86_64-unikraft-linux-musl", x86_64_unikraft_linux_musl),
 
+    ("riscv32ec-unknown-none-elf", riscv32ec_unknown_none_elf),
     ("riscv32i-unknown-none-elf", riscv32i_unknown_none_elf),
     ("riscv32im-unknown-none-elf", riscv32im_unknown_none_elf),
     ("riscv32imc-unknown-none-elf", riscv32imc_unknown_none_elf),
